498ff2ff0d8a3e14a7884dd2a2d85a8433580727
kawvin <285781427@qq.com>
2024-09-27 22:57:42 +0800

    left_right_Oled_2

--- boards/shields/display-091-drum/widgets/central_status.h
+++ boards/shields/display-091-drum/widgets/central_status.h
@@ -9,31 +9,15 @@
 
 #include <lvgl.h>
 #include <zephyr/kernel.h>
-// #include "util.h"
+#include "util.h"
 
 struct zmk_widget_status {
     sys_snode_t node;
     lv_obj_t *obj;
-    // lv_color_t cbuf[CANVAS_SIZE * CANVAS_SIZE];
-    // lv_color_t cbuf2[CANVAS_SIZE * CANVAS_SIZE];
-    // lv_color_t cbuf3[CANVAS_SIZE * CANVAS_SIZE];
+    lv_color_t cbuf[CANVAS_SIZE * CANVAS_SIZE];
+    lv_color_t cbuf2[CANVAS_SIZE * CANVAS_SIZE];
+    lv_color_t cbuf3[CANVAS_SIZE * CANVAS_SIZE];
     struct status_state state;
-};
-
-struct status_state {
-    uint8_t battery;
-    bool charging;
-#if !IS_ENABLED(CONFIG_ZMK_SPLIT) || IS_ENABLED(CONFIG_ZMK_SPLIT_ROLE_CENTRAL)
-    struct zmk_endpoint_instance selected_endpoint;
-    int active_profile_index;
-    bool active_profile_connected;
-    bool active_profile_bonded;
-    uint8_t layer_index;
-    const char *layer_label;
-    uint8_t wpm[10];
-#else
-    bool connected;
-#endif
 };
 
 int zmk_widget_status_init(struct zmk_widget_status *widget, lv_obj_t *parent);
--- boards/shields/display-091-drum/widgets/peripheral_status.h
+++ boards/shields/display-091-drum/widgets/peripheral_status.h
@@ -8,6 +8,7 @@
 
 #include <lvgl.h>
 #include <zephyr/kernel.h>
+#include "util.h"
 
 struct zmk_widget_peripheral_status {
     sys_snode_t node;
@@ -25,21 +26,6 @@ struct zmk_widget_status {
     struct status_state state;
 };
 
-struct status_state {
-    uint8_t battery;
-    bool charging;
-#if !IS_ENABLED(CONFIG_ZMK_SPLIT) || IS_ENABLED(CONFIG_ZMK_SPLIT_ROLE_CENTRAL)
-    struct zmk_endpoint_instance selected_endpoint;
-    int active_profile_index;
-    bool active_profile_connected;
-    bool active_profile_bonded;
-    uint8_t layer_index;
-    const char *layer_label;
-    uint8_t wpm[10];
-#else
-    bool connected;
-#endif
-};
 
 int zmk_widget_status_init(struct zmk_widget_status *widget, lv_obj_t *parent);
 lv_obj_t *zmk_widget_status_obj(struct zmk_widget_status *widget);
