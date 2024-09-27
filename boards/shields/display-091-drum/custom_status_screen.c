5c4cf566f839a716cb327425b499deb0991ce913
kawvin <285781427@qq.com>
2024-09-27 15:23:47 +0800

    left_right_Oled_1

--- boards/shields/display-091-drum/custom_status_screen.c
+++ boards/shields/display-091-drum/custom_status_screen.c
@@ -59,9 +59,9 @@ lv_obj_t *zmk_display_status_screen() {
     lv_obj_align(zmk_widget_bongo_drum_obj(&bongo_drum_widget), LV_ALIGN_TOP_LEFT, 80, 0);
 
     // Caps_Num_Scroll_Lock
-    #if IS_ENABLED(CONFIG_ZMK_HID_INDICATORS)
-    zmk_widget_hid_indicators_init(&hid_indicators_widget, screen);
-    lv_obj_align_to(zmk_widget_hid_indicators_obj(&hid_indicators_widget), LV_ALIGN_TOP_LEFT, 33,9);
+    //if IS_ENABLED(CONFIG_ZMK_HID_INDICATORS)
+    //zmk_widget_hid_indicators_init(&hid_indicators_widget, screen);
+    //lv_obj_align_to(zmk_widget_hid_indicators_obj(&hid_indicators_widget), LV_ALIGN_TOP_LEFT, 33,9);
     #endif
 
     // 电池电量
