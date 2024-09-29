32c0395610f1996182ef41156f8952f3afa8280b
kawvin <285781427@qq.com>
2024-09-29 19:33:00 +0800

    display_042_1

--- boards/shields/display-042/widgets/central_status.c
+++ boards/shields/display-042/widgets/central_status.c
@@ -47,8 +47,9 @@ int zmk_widget_status_init(struct zmk_widget_status *widget, lv_obj_t *parent) {
     //lv_obj_align(zmk_widget_modifiers_obj(&modifiers_widget), LV_ALIGN_BOTTOM_LEFT, 0, 0);
 
     // 层
-    //zmk_widget_layer_status_init(&layer_status_widget, widget->obj);
-    //lv_obj_align(zmk_widget_layer_status_obj(&layer_status_widget), LV_ALIGN_TOP_LEFT, 0,18);
+    zmk_widget_layer_status_init(&layer_status_widget, widget->obj);
+    // lv_obj_align(zmk_widget_layer_status_obj(&layer_status_widget), LV_ALIGN_TOP_LEFT, 0,18);
+    lv_obj_align(zmk_widget_layer_status_obj(&layer_status_widget), LV_ALIGN_TOP_LEFT, 0,0);
 
     // 敲鼓动画
     //zmk_widget_bongo_drum_init(&bongo_drum_widget, widget->obj);
@@ -71,8 +72,8 @@ int zmk_widget_status_init(struct zmk_widget_status *widget, lv_obj_t *parent) {
 // #endif
 
     // 输入输出状态
-    zmk_widget_output_status_init(&output_status_widget, widget->obj);
-    lv_obj_align(zmk_widget_output_status_obj(&output_status_widget), LV_ALIGN_TOP_LEFT, 0, 0);
+    //zmk_widget_output_status_init(&output_status_widget, widget->obj);
+    //lv_obj_align(zmk_widget_output_status_obj(&output_status_widget), LV_ALIGN_TOP_LEFT, 0, 0);
 
     return 0;
 }
