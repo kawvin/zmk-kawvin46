56ad698fe4ee5f1e98fda9c6ae6d055173c43978
kawvin <285781427@qq.com>
2024-09-29 19:26:14 +0800

    display_042_1

--- boards/shields/display-042/widgets/central_status.c
+++ boards/shields/display-042/widgets/central_status.c
@@ -47,8 +47,8 @@ int zmk_widget_status_init(struct zmk_widget_status *widget, lv_obj_t *parent) {
     //lv_obj_align(zmk_widget_modifiers_obj(&modifiers_widget), LV_ALIGN_BOTTOM_LEFT, 0, 0);
 
     // 层
-    zmk_widget_layer_status_init(&layer_status_widget, widget->obj);
-    lv_obj_align(zmk_widget_layer_status_obj(&layer_status_widget), LV_ALIGN_TOP_LEFT, 0,18);
+    //zmk_widget_layer_status_init(&layer_status_widget, widget->obj);
+    //lv_obj_align(zmk_widget_layer_status_obj(&layer_status_widget), LV_ALIGN_TOP_LEFT, 0,18);
 
     // 敲鼓动画
     //zmk_widget_bongo_drum_init(&bongo_drum_widget, widget->obj);
@@ -66,8 +66,8 @@ int zmk_widget_status_init(struct zmk_widget_status *widget, lv_obj_t *parent) {
 
 // 电池电量
 // #if IS_ENABLED(CONFIG_ZMK_WIDGET_BATTERY_STATUS_SHOW_PERIPHERAL)
-    zmk_widget_peripheral_battery_status_init(&peripheral_battery_status_widget, widget->obj);
-    lv_obj_align(zmk_widget_peripheral_battery_status_obj(&peripheral_battery_status_widget), LV_ALIGN_TOP_LEFT, 25, 0);
+    //zmk_widget_peripheral_battery_status_init(&peripheral_battery_status_widget, widget->obj);
+    //lv_obj_align(zmk_widget_peripheral_battery_status_obj(&peripheral_battery_status_widget), LV_ALIGN_TOP_LEFT, 25, 0);
 // #endif
 
     // 输入输出状态
